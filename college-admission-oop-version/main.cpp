b4e4fbdda0b1387ffdcc3d1215eac00676d4c8f5
CristinaNilvan <52707648+CristinaNilvan@users.noreply.github.com>
2021-07-20 19:41:38 +0300

    New updates
    Delete class from main.cpp file + others.

--- college-admission-oop-version/main.cpp
+++ college-admission-oop-version/main.cpp
@@ -1,88 +1,11 @@
 #include <iostream>
 #include <string>
+#include "student.h"
 
 #define NUMBER_OF_STUDENTS 6
 #define NUMBER_OF_POSITIONS 3
 
 using namespace std;
-
-class Student {
-private:
-    string Name;
-    float ExamGrade;
-    float MathGrade;
-    float BiggestGrade;
-    float BaccalaureateAverage;
-    int Mode;
-
-public:
-    Student() {
-        this->Name = "";
-        this->ExamGrade = 0;
-        this->MathGrade = 0;
-        this->BiggestGrade = 0;
-        this->BaccalaureateAverage = 0;
-        this->Mode = 0;
-    }
-
-    Student(string Name, float ExamGrade, float MathGrade, float BiggestGrade, float BaccalaureateAverage, int Mode) {
-        this->Name = move(Name);
-        this->ExamGrade = ExamGrade;
-        this->MathGrade = MathGrade;
-        this->BiggestGrade = BiggestGrade;
-        this->BaccalaureateAverage = BaccalaureateAverage;
-        this->Mode = Mode;
-    }
-
-    void SetName(string GivenName) {
-        Name = move(GivenName);
-    }
-    string GetName() {
-        return Name;
-    }
-
-    void SetExamGrade(float GivenGrade) {
-        ExamGrade = GivenGrade;
-    }
-    float GetExamGrade() {
-        return ExamGrade;
-    }
-
-    void SetMathGrade(float GivenGrade) {
-        MathGrade = GivenGrade;
-    }
-    float GetMathGrade() {
-        return MathGrade;
-    }
-
-    void SetBiggestGrade(float GivenGrade) {
-        BiggestGrade = GivenGrade;
-    }
-    float GetBiggestGrade() {
-        return BiggestGrade;
-    }
-
-    void SetBaccalaureateAverage(float GivenGrade) {
-        BaccalaureateAverage = GivenGrade;
-    }
-    float GetBaccalaureateAverage() {
-        return BaccalaureateAverage;
-    }
-
-    void SetMode(int GivenMode) {
-        Mode = GivenMode;
-    }
-    float GetMode() {
-        return Mode;
-    }
-
-    float CalculateCollegeAverage() const {
-        if (Mode == 1)
-            return (float)(0.8 * ExamGrade + 0.2 * BaccalaureateAverage);
-        else
-            return (float)(0.6 * ExamGrade + 0.2 * BiggestGrade + 0.2 * BaccalaureateAverage);
-    }
-};
 
 void SetDetails(Student Students[], int &NumberOfValidatedStudents) {
     string Names[NUMBER_OF_STUDENTS] = {"Keira Richelle", "Amy Dolph", "Braith Constance", "Darryl Marisa",
